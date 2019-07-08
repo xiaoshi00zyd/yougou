38c4d8f2d598d5fe4e562574690dc182e7d95874
cc710721 <52559963+cc710721@users.noreply.github.com>
2019-07-08 12:33:06 +0800

    find
    FIND

---
+++ src/components/find/components/hide.vue
@@ -0,0 +1,82 @@
+<template>
+    <div class="box" v-show="hide" >
+        <ul>
+            <li><a class="item1" href="">首页</a></li>
+            <li><a class="item2" href="">分类</a></li>
+            <li><a class="item3" href="">购物车</a></li>
+            <li><a class="item4" href="">我的优购</a></li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+  props: ['hide'],
+  data () {
+    return {
+    }
+  },
+  computed: {
+
+  },
+  created () {
+
+  },
+  mounted () {
+
+  },
+  watch: {
+
+  },
+  methods: {
+
+  },
+  components: {
+
+  }
+}
+</script>
+
+<style scoped>
+    .box {
+        width:100%;
+        height: 49px;
+        background: #666;
+    }
+    .box ul{
+        display: flex;
+        width: 100%;
+    }
+    .box ul li{
+        width:25%;
+        text-align: center
+    }
+    .box ul li a{
+        display: block;
+        font-size: 14px;
+        color: #fff;
+        background-position: center 9px;
+        background-size: auto 18px;
+        padding-top: 28px
+    }
+    .box ul li .item1 {
+        background: url(http://m.yougou.com/images/homec.png) no-repeat;
+        background-size: auto 18px;
+        background-position: center 9px;
+    }
+    .box ul li .item2 {
+       background: url(http://i1.ygimg.cn/pics/mobile/appointment/6.18Game/class.png) center 9px no-repeat;
+       background-size: auto 18px;
+        background-position: center 9px;
+    }
+    .box ul li .item3 {
+        background: url(http://m.yougou.com/images/ico-shopcartc.png) no-repeat;
+        background-size: auto 18px;
+        background-position: center 9px;
+    }
+    .box ul li .item4 {
+        background: url(http://m.yougou.com/images/ico-personc.png) no-repeat;
+        background-size: auto 18px;
+        background-position: center 9px;
+    }
+</style>
--- src/components/find/find.vue
+++ src/components/find/find.vue
@@ -1,22 +1,45 @@
 <template>
-    <div>
-        find
+    <div class="home">
+        <!-- 头部搜索 -->
+        <discoverNav @more='morea'></discoverNav>
+        <!-- 占头部的无内容空间 -->
+        <div class="empty"></div>
+        <!-- 头部右侧选项按钮效果 -->
+        <hide :hide='hide'></hide>
+        <!-- 商品展示列表 -->
+        <discoverList></discoverList>
     </div>
 </template>
-
 <script>
+import discoverNav from './components/discoverNav.vue'
+import discoverList from './components/discoverList.vue'
+import hide from './components/hide.vue'
 export default {
   data () {
     return {
-
+      bookProductList: [],
+      hide: false
     }
   },
   methods: {
-
+    morea () {
+      this.hide = !this.hide
+    }
+  },
+  created () {
+  },
+  components: {
+    discoverNav,
+    discoverList,
+    hide
   }
 }
 </script>
-
 <style scoped>
-
+.home{
+  background:#EEEEEE;
+}
+.empty {
+  height: 44px;
+}
 </style>
