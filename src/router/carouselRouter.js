f999015bb8b82d9b7dff5c550c74401c1bb2a9c7
ningningouba <3077207765@qq.com>
2019-07-09 19:59:58 +0800

    10088

--- src/router/carouselRouter.js
+++ src/router/carouselRouter.js
@@ -3,16 +3,28 @@ export default [
   {
     name: 'carouselOne',
     path: '/carouselOne',
-    component: () => import('../components/home/components/carousel/carouselOne.vue')
+    component: () => import('../components/home/components/carousel/carouselOne.vue'),
+    meta: {
+      title: '清凉季 至高减150元',
+      cache: false
+    }
   },
   {
     name: 'carouselTwo',
     path: '/carouselTwo',
-    component: () => import('../components/home/components/carousel/carouselTwo.vue')
+    component: () => import('../components/home/components/carousel/carouselTwo.vue'),
+    meta: {
+      title: '夏季人气凉鞋 低至1.5折',
+      cache: false
+    }
   },
   {
     name: 'carouselThree',
     path: '/carouselThree',
-    component: () => import('../components/home/components/carousel/carouselThree.vue')
+    component: () => import('../components/home/components/carousel/carouselThree.vue'),
+    meta: {
+      title: '运动热力单品 78元起',
+      cache: false
+    }
   }
 ]
