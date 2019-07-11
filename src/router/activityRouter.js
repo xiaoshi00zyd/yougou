036f9202351a6d7e23e95019b6fc325144802a17
ningningouba <3077207765@qq.com>
2019-07-11 15:10:52 +0800

    最后一次路由表更改

--- src/router/activityRouter.js
+++ src/router/activityRouter.js
@@ -1,8 +1,8 @@
 
 export default [
   {
-    name: 'activity',
-    path: '/activity',
+    name: 'activity0',
+    path: '/activity0',
     component: () => import('../components/home/components/activity/activity0.vue')
   }
 ]
